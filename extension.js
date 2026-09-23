b117996a86d00116cedd72f6e301a8d0fb7de578
Cubes, Studio. <inakuu69@gmail.com>
2026-09-23 15:20:53 -0600

    Fix Silicon for Gandi remote extension API

--- extension.js
+++ extension.js
@@ -1,210 +1,181 @@
-class Silicon {
-    constructor(runtime) {
-        this.runtime = runtime;
+(function (Scratch) {
+    "use strict";
 
-        this.loader = "Fabric";
-        this.loaded = false;
-        this.projectName = "";
-        this.loaderVersion = "1.0.0";
-        this.status = "Not loaded";
-    }
-
-    getInfo() {
-        return {
-            id: "silicon",
-            name: "Silicon",
-            color1: "#4b8cff",
-            color2: "#3266c7",
-            color3: "#244c99",
-
-            blocks: [
-                {
-                    opcode: "loadProject",
-                    blockType: "command",
-                    text: "load project with [LOADER]",
-                    arguments: {
-                        LOADER: {
-                            type: "string",
-                            menu: "loaders",
-                            defaultValue: "Fabric"
-                        }
-                    }
-                },
-
-                {
-                    opcode: "setLoader",
-                    blockType: "command",
-                    text: "set loader to [LOADER]",
-                    arguments: {
-                        LOADER: {
-                            type: "string",
-                            menu: "loaders",
-                            defaultValue: "Fabric"
-                        }
-                    }
-                },
-
-                {
-                    opcode: "reloadProject",
-                    blockType: "command",
-                    text: "reload project"
-                },
-
-                {
-                    opcode: "unloadProject",
-                    blockType: "command",
-                    text: "unload project"
-                },
-
-                {
-                    opcode: "isLoaded",
-                    blockType: "Boolean",
-                    text: "project loaded?"
-                },
-
-                {
-                    opcode: "getLoader",
-                    blockType: "reporter",
-                    text: "current loader"
-                },
-
-                {
-                    opcode: "getStatus",
-                    blockType: "reporter",
-                    text: "Silicon status"
-                },
-
-                {
-                    opcode: "getLoaderVersion",
-                    blockType: "reporter",
-                    text: "loader version"
-                },
-
-                {
-                    opcode: "getProjectName",
-                    blockType: "reporter",
-                    text: "loaded project name"
-                },
-
-                {
-                    opcode: "loaderSupports",
-                    blockType: "Boolean",
-                    text: "[LOADER] supported?"
-                }
-            ],
-
-            menus: {
-                loaders: {
-                    acceptReporters: true,
-                    items: [
-                        "Fabric",
-                        "NeoVirus"
-                    ]
-                }
-            }
-        };
-    }
-
-    setLoader(args) {
-        const loader = String(args.LOADER);
-
-        if (!this.loaderSupports({ LOADER: loader })) {
-            this.status = "Unknown loader";
-            return;
-        }
-
-        this.loader = loader;
-        this.loaded = false;
-        this.status = loader + " selected";
-    }
-
-    loadProject(args) {
-        const loader = String(args.LOADER);
-
-        if (!this.loaderSupports({ LOADER: loader })) {
+    class Silicon {
+        constructor() {
+            this.loader = "Fabric";
             this.loaded = false;
-            this.status = "Unknown loader";
-            return;
+            this.projectName = "Gandi Project";
+            this.loaderVersion = "1.0.0";
+            this.status = "Silicon ready";
         }
 
-        this.loader = loader;
+        getInfo() {
+            return {
+                id: "silicon",
+                name: "Silicon",
+                color1: "#4b8cff",
+                color2: "#3266c7",
+                color3: "#244c99",
 
-        // Try to obtain the project name from the VM.
-        try {
-            if (this.runtime && this.runtime.getTargetForStage) {
-                const stage = this.runtime.getTargetForStage();
+                blocks: [
+                    {
+                        opcode: "loadProject",
+                        blockType: Scratch.BlockType.COMMAND,
+                        text: "load project with [LOADER]",
+                        arguments: {
+                            LOADER: {
+                                type: Scratch.ArgumentType.STRING,
+                                menu: "loaders",
+                                defaultValue: "Fabric"
+                            }
+                        }
+                    },
+                    {
+                        opcode: "setLoader",
+                        blockType: Scratch.BlockType.COMMAND,
+                        text: "set loader to [LOADER]",
+                        arguments: {
+                            LOADER: {
+                                type: Scratch.ArgumentType.STRING,
+                                menu: "loaders",
+                                defaultValue: "Fabric"
+                            }
+                        }
+                    },
+                    {
+                        opcode: "reloadProject",
+                        blockType: Scratch.BlockType.COMMAND,
+                        text: "reload project"
+                    },
+                    {
+                        opcode: "unloadProject",
+                        blockType: Scratch.BlockType.COMMAND,
+                        text: "unload project"
+                    },
+                    {
+                        opcode: "isLoaded",
+                        blockType: Scratch.BlockType.BOOLEAN,
+                        text: "project loaded?"
+                    },
+                    {
+                        opcode: "getLoader",
+                        blockType: Scratch.BlockType.REPORTER,
+                        text: "current loader"
+                    },
+                    {
+                        opcode: "getStatus",
+                        blockType: Scratch.BlockType.REPORTER,
+                        text: "Silicon status"
+                    },
+                    {
+                        opcode: "getLoaderVersion",
+                        blockType: Scratch.BlockType.REPORTER,
+                        text: "loader version"
+                    },
+                    {
+                        opcode: "getProjectName",
+                        blockType: Scratch.BlockType.REPORTER,
+                        text: "loaded project name"
+                    },
+                    {
+                        opcode: "loaderSupported",
+                        blockType: Scratch.BlockType.BOOLEAN,
+                        text: "[LOADER] supported?",
+                        arguments: {
+                            LOADER: {
+                                type: Scratch.ArgumentType.STRING,
+                                menu: "loaders",
+                                defaultValue: "Fabric"
+                            }
+                        }
+                    }
+                ],
 
-                if (stage && stage.getName) {
-                    this.projectName = stage.getName();
+                menus: {
+                    loaders: {
+                        acceptReporters: true,
+                        items: ["Fabric", "NeoVirus"]
+                    }
                 }
+            };
+        }
+
+        setLoader(args) {
+            const loader = String(args.LOADER);
+
+            if (!this.loaderSupported({ LOADER: loader })) {
+                this.status = "Unknown loader";
+                return;
             }
-        } catch (e) {
+
+            this.loader = loader;
+            this.loaded = false;
+            this.status = loader + " selected";
+        }
+
+        loadProject(args) {
+            const loader = String(args.LOADER);
+
+            if (!this.loaderSupported({ LOADER: loader })) {
+                this.loaded = false;
+                this.status = "Unknown loader";
+                return;
+            }
+
+            this.loader = loader;
+            this.loaded = true;
+            this.projectName = "Gandi Project";
+            this.status = "Loaded with " + loader;
+        }
+
+        reloadProject() {
+            if (!this.loaded) {
+                this.status = "No project loaded";
+                return;
+            }
+
+            const loader = this.loader;
+            this.loaded = false;
+            this.status = "Reloading with " + loader + "...";
+
+            setTimeout(() => {
+                this.loaded = true;
+                this.status = "Loaded with " + loader;
+            }, 100);
+        }
+
+        unloadProject() {
+            this.loaded = false;
+            this.status = "Project unloaded";
             this.projectName = "";
         }
 
-        if (!this.projectName) {
-            this.projectName = "Gandi Project";
+        isLoaded() {
+            return this.loaded;
         }
 
-        this.loaded = true;
-        this.status = "Loaded with " + this.loader;
-    }
-
-    reloadProject() {
-        if (!this.loaded) {
-            this.status = "No project loaded";
-            return;
+        getLoader() {
+            return this.loader;
         }
 
-        const currentLoader = this.loader;
+        getStatus() {
+            return this.status;
+        }
 
-        this.loaded = false;
-        this.status = "Reloading with " + currentLoader + "...";
+        getLoaderVersion() {
+            return this.loaderVersion;
+        }
 
-        // Small asynchronous delay so the reload behaves like a loader.
-        setTimeout(() => {
-            this.loaded = true;
-            this.status = "Loaded with " + currentLoader;
-        }, 100);
+        getProjectName() {
+            return this.projectName;
+        }
+
+        loaderSupported(args) {
+            const loader = String(args.LOADER).toLowerCase();
+            return loader === "fabric" || loader === "neovirus";
+        }
     }
 
-    unloadProject() {
-        this.loaded = false;
-        this.status = "Project unloaded";
-        this.projectName = "";
-    }
-
-    isLoaded() {
-        return this.loaded;
-    }
-
-    getLoader() {
-        return this.loader;
-    }
-
-    getStatus() {
-        return this.status;
-    }
-
-    getLoaderVersion() {
-        return this.loaderVersion;
-    }
-
-    getProjectName() {
-        return this.projectName;
-    }
-
-    loaderSupports(args) {
-        const loader = String(args.LOADER).toLowerCase();
-
-        return (
-            loader === "fabric" ||
-            loader === "neovirus"
-        );
-    }
-}
-
-(function() {
-    const extension = new Silicon(runtime);
-    runtime.registerExtension(extension);
-})();
+    Scratch.extensions.register(new Silicon());
+})(Scratch);
